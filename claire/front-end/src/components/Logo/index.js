5c077c7ad5f85b4cd444eaac52b8334614031325
Claire Miller <claire.isabella.miller@gmail.com>
2019-03-22 18:45:21 -0400

    Assignment for 3/22/2019

--- claire/front-end/src/components/Logo/index.js
+++ claire/front-end/src/components/Logo/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import styles from './index.module.css'
-import Logo from '../Logo'
 
-export default ({ children }) => (
+export default () => (
   <div className={styles.container}>
-    {children}
-    <Logo />
+    El Backo
   </div>
 )
