2ca4d411fcdd45b4a7ec93a005eb565c354989d6
SYD-LENOVO\Owner <sydney.elaine.miller@gmail.com>
2019-04-03 14:40:58 -0400

    BLAHB LAH

---
+++ sydney/front-end/src/components/SingleplayerPlayerNames/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import _ from 'lodash'
+import { Formik, Field } from 'formik'
+import MenuContainer from '../MenuContainer'
+import initialValues from './lib/initialValues'
+import styles from './index.module.css'
+import Button from '../Button'
+
+
+
+export default ({
+  match: {
+    params: {
+      playersCount,
+    },
+  },
+}) => (
+  <MenuContainer>
+    <Formik
+      initialValues={initialValues(playersCount)}
+      onSubmit={(variables) => (
+        console.log('Form submitted with:', variables)
+      )}
+    >
+      {({ values: { players }, handleSubmit, errors }) => (
+        <form onSubmit={handleSubmit}>
+          {_.map(players, ({ number }, index) => (
+            <div key={index}>
+              <Field
+                name={`players.${index}.name`}
+                className={styles.field}
+                placeholder={`Player ${index + 1} name`}
+              />
+                        <Button component='button' type='submit'>
+            Start game
+          </Button>
+
+            </div>
+          ))}
+        </form>
+      )}
+    </Formik>
+  </MenuContainer>
+)
+
+
+
